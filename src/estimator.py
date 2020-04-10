1dda43818358cd5fcdf03101d112fb1bd58a0cfc
eric <rcmacharia7@gmail.com>
2020-04-10 14:53:39 +0300

    Fix a data bug

--- src/estimator.py
+++ src/estimator.py
@@ -1,16 +1,16 @@
-sample = {
-    'region': {
-        'name': 'Africa',
-        'avgAge': 19.7,
-        'avgDailyIncomeInUSD': 5,
-        'avgDailyIncomePopulation': 0.71,
-    },
-    'periodType': 'days',
-    'timeToElapse': 58,
-    'reportedCases': 674,
-    'population': 66622705,
-    'totalHospitalBeds': 1380614
-}
+# sample = {
+#     'region': {
+#         'name': 'Africa',
+#         'avgAge': 19.7,
+#         'avgDailyIncomeInUSD': 5,
+#         'avgDailyIncomePopulation': 0.71,
+#     },
+#     'periodType': 'days',
+#     'timeToElapse': 58,
+#     'reportedCases': 674,
+#     'population': 66622705,
+#     'totalHospitalBeds': 1380614
+# }
 
 
 def normalize_days(data):
@@ -61,5 +61,5 @@ def estimator(data):
     return estimate
 
 
-print(estimator(sample))
+# print(estimator(sample))
 
