66b41ae063b4cd8a7d0a0c594e35eff70f3ec342
eric <rcmacharia7@gmail.com>
2020-04-10 16:42:31 +0300

    Fix output data format

--- src/estimator.py
+++ src/estimator.py
@@ -1,28 +1,26 @@
-# sample = {
-#     'region': {
-#         'name': 'Africa',
-#         'avgAge': 19.7,
-#         'avgDailyIncomeInUSD': 5,
-#         'avgDailyIncomePopulation': 0.71,
-#     },
-#     'periodType': 'days',
-#     'timeToElapse': 58,
-#     'reportedCases': 674,
-#     'population': 66622705,
-#     'totalHospitalBeds': 1380614
-# }
+sample = {
+    'region': {
+        'name': 'Africa',
+        'avgAge': 19.7,
+        'avgDailyIncomeInUSD': 5,
+        'avgDailyIncomePopulation': 0.71,
+    },
+    'periodType': 'days',
+    'timeToElapse': 58,
+    'reportedCases': 674,
+    'population': 66622705,
+    'totalHospitalBeds': 1380614
+}
 
 
 def normalize_days(data):
     if data['periodType'] == 'days':
         days = data['timeToElapse']
-        return days
-    if data['periodType'] == 'weeks':
+    elif data['periodType'] == 'weeks':
         days = data['timeToElapse'] * 7
-        return days
-    if data['periodType'] == 'months':
+    elif data['periodType'] == 'months':
         days = data['timeToElapse'] * 30
-        return days
+    return days
 
 
 def estimation(data):
@@ -36,8 +34,10 @@ def estimation(data):
     severeImpact['currentlyInfected'] = severeImpactCases
     estimate = {
         'data': data,
-        'impact': impact,
-        'severeImpact': severeImpact
+        'estimate': {
+            'impact': impact,
+            'severeImpact': severeImpact
+        },
     }
     return estimate
 
@@ -46,13 +46,14 @@ def estimationByTime(data):
     estimate = estimation(data)
     days = normalize_days(data)
 
-    currentlyInfectedImpactByTime = estimate['impact']['currentlyInfected'] * (
-        2 * int(days/3))
-    currentlyInfectedSevereByTime = estimate['severeImpact']['currentlyInfected'] * (
+    currentlyInfectedImpactByTime = estimate['estimate']['impact']['currentlyInfected'] * (
         2 * int(days/3))
 
-    estimate['impact']['infectionsByRequestedTime'] = currentlyInfectedImpactByTime
-    estimate['severeImpact']['infectionsByRequestedTime'] = currentlyInfectedSevereByTime
+    currentlyInfectedSevereByTime = estimate['estimate']['severeImpact']['currentlyInfected'] * (
+        2 * int(days/3))
+
+    estimate['estimate']['severeImpact']['infectionsByRequestedTime'] = currentlyInfectedSevereByTime
+    estimate['estimate']['impact']['infectionsByRequestedTime'] = currentlyInfectedImpactByTime
     return estimate
 
 
@@ -61,5 +62,4 @@ def estimator(data):
     return estimate
 
 
-# print(estimator(sample))
-
+print(estimator(sample))
